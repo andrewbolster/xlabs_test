08cca59887c27237b162370624c5dae1c242c090
Andrew Bolster <me@andrewbolster.info>
2016-09-07 14:49:52 +0100

    yflip and optional split

--- xlabs_test/__init__.py
+++ xlabs_test/__init__.py
@@ -19,7 +19,7 @@ def unique_cm_dict_from_list(items, basecmap = 'gist_rainbow'):
 def build_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("xlabscsv", help="XLabs Output CSV for Visualisation")
-    parser.add_argument("-c", "--calicut", action='store', default=800, help="Estimated index of the end of Calibration")
+    parser.add_argument("-c", "--calicut", action='store', default=None, help="Estimated index of the end of Calibration")
     parser.add_argument("-x", "--xlim", action='store', default=1920, help="Restrict view to given x limit")
     parser.add_argument("-y", "--ylim", action='store', default=1080, help="Restrict view to given y limit")
     parser.add_argument("--xslim", action='store', default=1, help="Restrict view to given x limit for the scaled view")
@@ -29,7 +29,7 @@ def build_argparser():
 
     return parser
 
-def go(xlabscsv, calicut, invert_y=False, xlim=None, ylim=None, xslim=None, yslim=None, confidence_factor=None, **kwargs):
+def go(xlabscsv, calicut=None, invert_y=False, xlim=None, ylim=None, xslim=None, yslim=None, confidence_factor=None, **kwargs):
     df = pd.read_csv(xlabscsv,
                      names=["Xr", "Yr", "Xp", "Xs", "Yp", "Ys", "C", 3, ],
                      index_col=False)
@@ -37,27 +37,33 @@ def go(xlabscsv, calicut, invert_y=False, xlim=None, ylim=None, xslim=None, ysli
     if confidence_factor is not None:
         df = df[df.C < df.C.quantile(confidence_factor)]
 
+    if calicut is not None:
+        fig, sup_axes = plt.subplots(2,3, figsize = (16,10))
+    else:
+        fig, sup_axes = plt.subplots(1,3, figsize = (16,10))
 
-    fig, axes = plt.subplots(2,3, figsize = (16,10))
 
 
     for i,(x,y) in enumerate(zip(['Xr','Xp', 'Xs'],['Yr','Yp', 'Ys'])):
         _df = df.loc[:calicut]
+        axes=sup_axes
         _df.plot.scatter(x=x, y=y, c=list(unique_cm_dict_from_list(_df.index.tolist()).values()),
-                        ax=axes[0][i])
+                        ax=axes[i])
 
 
+        if calicut is not None:
+            _df = df.loc[calicut:]
+            axes=sup_axes
+            _df.plot.scatter(x=x, y=y, c=list(unique_cm_dict_from_list(_df.index.tolist()).values()),
+                             ax=axes[1][i])
 
-        _df = df.loc[calicut:]
-
-
-        _df.plot.scatter(x=x, y=y, c=list(unique_cm_dict_from_list(_df.index.tolist()).values()),
-                         ax=axes[1][i])
+    if calicut is not None:
+        axes = sup_axes
+    else:
+        axes = [sup_axes]
 
     for row, _axes in enumerate(axes):
         for col, ax in enumerate(_axes):
-            if invert_y:
-                ax.invert_yaxis()
             if col is not 2:
                 if xlim is not None:
                     ax.set_xlim(0,xlim)
@@ -68,6 +74,8 @@ def go(xlabscsv, calicut, invert_y=False, xlim=None, ylim=None, xslim=None, ysli
                     ax.set_xlim(0, xslim)
                 if yslim is not None:
                     ax.set_ylim(0, yslim)
+            if invert_y:
+                ax.invert_yaxis()
 
     pad=5
     for ax, col in zip(axes[0], ['Raw','Processed','Scaled']):
@@ -75,10 +83,11 @@ def go(xlabscsv, calicut, invert_y=False, xlim=None, ylim=None, xslim=None, ysli
                     xycoords='axes fraction', textcoords='offset points',
                     size='large', ha='center', va='baseline')
 
-    for ax, row in zip(axes[:, 0], ['Calibration','Post-Calibration']):
-        ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
-                    xycoords=ax.yaxis.label, textcoords='offset points',
-                    size='large', ha='right', va='center', rotation='vertical')
+    if calicut is not None:
+        for ax, row in zip(axes[:, 0], ['Calibration','Post-Calibration']):
+            ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
+                        xycoords=ax.yaxis.label, textcoords='offset points',
+                        size='large', ha='right', va='center', rotation='vertical')
 
     fig.suptitle(xlabscsv)
     plt.show()
