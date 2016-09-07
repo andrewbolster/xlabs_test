8a12cf7a87279cf4c035d91ad037e5e05e8ab569
Andrew Bolster <me@andrewbolster.info>
2016-09-07 17:36:26 +0100

    colorbar fix and text rotation

--- xlabs_test/__init__.py
+++ xlabs_test/__init__.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.colorbar import ColorbarBase
 plt.rcParams['image.cmap'] = 'gist_ncar'  # change default colormap
 
 import matplotlib.patches as patches
@@ -53,11 +54,13 @@ def go(xlabscsv, calicut=None, invert_y=False, xlim=None, ylim=None, xslim=None,
     except:
         raise
 
+    colorlist, scalarmap = unique_cm_dict_from_list(df.index.tolist())
+    df['color'] = colorlist
+    cm  = 'gist_ncar'
+
     if confidence_factor is not None:
         df = df[df.C < df.C.quantile(confidence_factor)]
 
-    colorlist, scalarmap = unique_cm_dict_from_list(df.index.tolist())
-    df['color'] = colorlist
 
 
     fig, axes = plt.subplots(nrows = len(split_indexes), ncols=3, figsize= (16,10))
@@ -65,9 +68,15 @@ def go(xlabscsv, calicut=None, invert_y=False, xlim=None, ylim=None, xslim=None,
     if len(split_indexes)==1:
         axes=[axes]
 
+    fig.suptitle(xlabscsv)
+    fig.subplots_adjust(right=0.925)
+    cbar_ax = fig.add_axes([0.95,0.05,0.01,0.9])
+    scalarmap._A = df.index.values
+
     for i, (lower,upper) in enumerate(split_indexes):
         for j,(x,y) in enumerate(zip(['Xr','Xp', 'Xs'],['Yr','Yp', 'Ys'])):
-            df.loc[lower:upper].plot.scatter(x=x, y=y, c='index', ax=axes[i][j], colormap='gist_ncar', colorbar=True)
+            _df=df.loc[lower:upper]
+            _df.plot.scatter(x=x, y=y, c=[scalarmap.to_rgba(c) for c in _df.index.values], ax=axes[i][j])
 
 
     for row, _axes in enumerate(axes):
@@ -99,11 +108,13 @@ def go(xlabscsv, calicut=None, invert_y=False, xlim=None, ylim=None, xslim=None,
         for ax, row in zip(axes[:, 0], ["{} to {}".format(lower, upper) for lower,upper in split_indexes]):
             ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
                         xycoords=ax.yaxis.label, textcoords='offset points',
-                        size='large', ha='right', va='center', rotation='vertical')
+                        size='large', ha='right', va='center', rotation=320)
 
-    fig.suptitle(xlabscsv)
-    fig.subplots_adjust(right=0.9)
-    #cbar_ax = fig.add_axes([0.85,0.05,0.8,0.9])
+
+    #plt.clim(0, df.index.max())
+    scalarmap._A=[]
+    plt.colorbar(scalarmap, cax=cbar_ax)
+    cbar_ax.invert_yaxis()
 
     plt.show()
 
